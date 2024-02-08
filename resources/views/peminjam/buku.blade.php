3938d473c345fd4513f3ce9da09ffdffc9fd87c8
ca-kraa <alexandrokingnabil@gmail.com>
2024-02-08 11:05:57 +0700

    Perpustakaan Digital

--- resources/views/peminjam/buku.blade.php
+++ resources/views/peminjam/buku.blade.php
@@ -2,19 +2,22 @@
 @section('title', 'Buku')
 @section('Dashboard Untuk', 'Peminjam')
 
-
 @section('konten')
     <div class="container-fluid py-4">
         <div class="row">
             <div class="col-12">
                 <div class="card my-4">
                     <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                        <div class="bg-gradient-info  shadow-info  border-radius-lg pt-4 pb-3">
+                        <div class="bg-gradient-info shadow-info border-radius-lg pt-4 pb-3">
                             <h6 class="text-white text-capitalize ps-3 mb-0 animate__bounce">Buku</h6>
                         </div>
                     </div>
                     <div class="card-body px-0 pb-2">
                         <div class="table-responsive p-0">
+                            <div class="input-group input-group-outline my-3 ml-2" style="max-width: 300px;">
+                                <label class="form-label">Search</label>
+                                <input type="search" class="form-control">
+                            </div>
                             <table class="table align-items-center mb-0" id="tabelBuku">
                                 <thead>
                                     <tr>
