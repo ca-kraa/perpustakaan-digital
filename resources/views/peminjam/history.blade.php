77bd63a6223d2690ce67a3c2557d65d4581af467
ca-kraa <alexandrokingnabil@gmail.com>
2024-02-10 11:35:31 +0700

    Perpustakaan Digital

--- resources/views/peminjam/history.blade.php
+++ resources/views/peminjam/history.blade.php
@@ -9,7 +9,7 @@
                 <div class="card my-4">
                     <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                         <div class="bg-gradient-info shadow-info border-radius-lg pt-4 pb-3">
-                            <h6 class="text-white text-capitalize ps-3 mb-0 animate__bounce">Buku</h6>
+                            <h6 class="text-white text-capitalize ps-3 mb-0 animate__bounce">History</h6>
                         </div>
                     </div>
                     <div class="card-body px-0 pb-2">
@@ -21,16 +21,13 @@
                                             Judul</th>
                                         <th
                                             class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
-                                            Penulis</th>
+                                            Tanggal Peminjaman</th>
                                         <th
                                             class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
-                                            Penerbit</th>
+                                            Tanggal Pengembalian</th>
                                         <th
                                             class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
-                                            Tahun Terbit</th>
-                                        <th
-                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
-                                        </th>
+                                            Status Peminjam</th>
                                     </tr>
                                 </thead>
                                 <tbody id="bukuTableBody" class="display">
@@ -42,4 +39,51 @@
             </div>
         </div>
     </div>
+
+    <script src="{{ asset('assets/cdn') }}/jquery.js"></script>
+    <script src="{{ asset('assets/cdn') }}/bootstrap.bundle.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $.get('/show-peminjam-user', function(data) {
+                $.each(data, function(index, item) {
+                    var buttonColor;
+                    if (item.status_peminjam === 'Pending') {
+                        buttonColor = 'btn-info';
+                    } else if (item.status_peminjam === 'Sudah Di Kembalikan') {
+                        buttonColor = 'btn-success';
+                    } else if (item.status_peminjam === 'Belum Di Kembalikan') {
+                        buttonColor = 'btn-danger';
+                    }
+
+                    $('#bukuTableBody').append(`
+                    <tr>
+                        <td class="align-middle">
+                            <div class="d-flex px-2 py-1">
+                                <h6 class="mt-2 ml-4 text-sm">${item.judul_buku}</h6>
+                            </div>
+                        </td>
+                        <td class="align-middle">
+                            <div class="d-flex px-2 py-1">
+                                <h6 class="mt-2 ml-4 text-sm">${item.tanggal_peminjaman}</h6>
+                            </div>
+                        </td>
+                        <td class="align-middle">
+                            <div class="d-flex px-2 py-1">
+                                <h6 class="mt-2 ml-4 text-sm">${item.tanggal_pengembalian}</h6>
+                            </div>
+                        </td>
+                        <td class="align-middle">
+                            <div class="d-flex px-2 py-1">
+                                <h6 class="mt-2 ml-4 text-sm"><button class="btn ${buttonColor}">${item.status_peminjam}</button></h6>
+                            </div>
+                        </td>
+                    </tr>
+                    `);
+                });
+            });
+        });
+    </script>
+
+
 @endsection
